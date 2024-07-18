dfb060a36b9e013664c25f568d5321c2e5273972
VLASUSDT <vlasusdt@MacBook-Air-VLASUSDT.local>
2024-07-18 14:11:31 +0300

    Randomize update

--- utils/generateRandomLogin.js
+++ utils/generateRandomLogin.js
@@ -1,5 +1,17 @@
-import { generate as generateRandomWord, count as randomWordCount } from 'random-words';
+import { generate as generateRandomWord } from 'random-words';
+import { count as randomWordsCount } from 'random-words';
+
+// Function to generate a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export function generateRandom() {
-    return generateRandomWord() + randomWordCount();
+    const randomWord = generateRandomWord();
+    const totalWordsCount = randomWordsCount();
+    const randomNumber = getRandomInt(1, totalWordsCount); // Generate a random number
+
+    return `${randomWord}${randomNumber}`;
 }
